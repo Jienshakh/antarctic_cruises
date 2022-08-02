8776d3fb170ef7d1fb6fc53c5095ad9f59764faa
Jienshakh <jeydin@mail.ru>
2022-08-02 20:29:08 +0900

    second_attempt

--- source/js/utils/menu-toggle.js
+++ source/js/utils/menu-toggle.js
@@ -1,21 +1,21 @@
-const navMain = document.querySelector('.main-nav__site-menu');
+const navMain = document.querySelector('.main-nav');
 const navToggle = document.querySelector('.main-nav__toggle');
 const headerLogo = document.querySelector('.page-header__logo');
-const headerTitle = document.querySelector('.page-header__title');
-const headerTagline = document.querySelector('.page-header__tagline');
+const headerTitle = document.querySelector('.page-header h2');
+const headerTagline = document.querySelector('.page-header p');
 
-navMain.classList.remove('main-nav__site-menu--nojs');
+navMain.classList.remove('main-nav--nojs');
 
 navToggle.addEventListener('click', function () {
-  if (navMain.classList.contains('main-nav__site-menu--closed')) {
-    navMain.classList.remove('main-nav__site-menu--closed');
-    navMain.classList.add('main-nav__site-menu--opened');
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
     headerLogo.style.display = 'none';
     headerTitle.style.display = 'none';
     headerTagline.style.display = 'none';
   } else {
-    navMain.classList.add('main-nav__site-menu--closed');
-    navMain.classList.remove('main-nav__site-menu--opened');
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
     headerLogo.style.display = 'block';
     headerTitle.style.display = 'block';
     headerTagline.style.display = 'block';
